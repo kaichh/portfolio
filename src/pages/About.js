897192cc1105d3be82fcec5aa8e6ccbb700561bf
KC Huang <kchuang@KCdeMacBook-Air.local>
2022-10-04 21:14:41 -0500

    3. Finish 3 section + 1 Style component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const About = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default About;
