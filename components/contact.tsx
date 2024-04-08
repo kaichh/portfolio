12b59fb3dd56e919e163f1de7872df7c73da6b94
kaichh <huangkc3@gmail.com>
2024-04-07 21:34:40 -0500

    feat: add dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
       viewport={{ once: true }}
     >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:kc.huang@tamu.edu">
           kc.huang@tamu.edu
@@ -31,7 +31,7 @@ export default function Contact() {
       </p>
 
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
@@ -42,7 +42,8 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack focus:outline-none focus:ring-0 focus:border-gray-900"
+          className="h-14 px-4 rounded-lg borderBlack focus:outline-none focus:ring-0 focus:border-gray-900
+                    dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           required
@@ -50,7 +51,8 @@ export default function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 focus:outline-none focus:ring-0 focus:border-gray-900"
+          className="h-52 my-3 rounded-lg borderBlack p-4 focus:outline-none focus:ring-0 focus:border-gray-900
+                  dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
                     focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border
-                     border-black/10"
+                     border-black/10 dark:bg-white/10"
           href="/resume.pdf"
           download
         >
@@ -99,7 +99,9 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] 
+                      hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer 
+                      borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/kaichihhuang/"
           target="_blank"
         >
@@ -107,7 +109,9 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full 
+                      focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition 
+                      cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/kaichh"
           target="_blank"
         >
